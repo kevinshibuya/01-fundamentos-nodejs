805d2450524d174bf7239e4c01f415b8dc2b828b
Kevin Borges <kevin.borges@gruporbs.com.br>
2024-05-21 15:54:46 -0300

    http methods and routes

--- src/server.js
+++ src/server.js
@@ -1,6 +1,16 @@
 import http from "node:http";
 
 const server = http.createServer((req, res) => {
+  const { method, url } = req;
+
+  if (method === "GET" && url === "/users") {
+    return res.end("lista");
+  }
+
+  if (method === "POST" && url === "/users") {
+    return res.end("criação");
+  }
+
   return res.end("hello");
 });
 
