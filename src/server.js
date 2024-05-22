b4c42105fcc0d9bbb152ae8788f3e77650cd4714
Kevin Borges <kevin.borges@gruporbs.com.br>
2024-05-22 08:58:19 -0300

    statefull server and headers

--- src/server.js
+++ src/server.js
@@ -1,13 +1,22 @@
 import http from "node:http";
 
+const users = [];
+
 const server = http.createServer((req, res) => {
   const { method, url } = req;
 
   if (method === "GET" && url === "/users") {
-    return res.end("lista");
+    return res
+      .setHeader("Content-type", "application/json")
+      .end(JSON.stringify(users));
   }
 
   if (method === "POST" && url === "/users") {
+    users.push({
+      id: 1,
+      name: "kevin shibuya",
+      email: "kevinshibuya@hotmail.com",
+    });
     return res.end("criação");
   }
 
